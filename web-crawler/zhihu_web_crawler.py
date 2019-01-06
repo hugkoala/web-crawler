1c73779cba6a868b7ac5fb22f8c5827222feafd7
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-07 00:52:12 +0900

    add ZhihuWebCrawler Class

---
+++ web-crawler/zhihu_web_crawler.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import json
+import os
+import requests
+
+class ZhihuWebCrawler(object):
+
+
+    ZHIHU_URL = 'https://www.zhihu.com/'
+
+    """docstring for ZhihuWebCrawler"""
+    def __init__(self, cmdline=None):
+
+
+    def parse_articles(self, board, path='.', timeout=5):
+        filename = board + '.json'
+        filename = os.path.join(path, filename)
+        print('Processing:')
+        resp = requests.get(
+            url = self.ZHIHU_URL + '/topic#'
+        )
+        self.store(filename, u'{"articles": [', 'w')
+
+        self.store(filename, u']}', 'a')
+        return filename
+
+    # def parse_article(self, link, board):
+
+
+    @staticmethod
+    def store(filename, data, mode):
+        with codecs.open(filename, mode, encoding='utf-8') as f:
+            f.write(data)
+
+    @staticmethod
+    def get(filename, mode='r'):
+        with codecs.open(filename, mode, encoding='utf-8') as f:
+            return json.load(f)
