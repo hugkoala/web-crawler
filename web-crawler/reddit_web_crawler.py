31447074f60d1d609e92476affcca8be05ad410b
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-08 01:16:00 +0900

    select articles

--- web-crawler/reddit_web_crawler.py
+++ web-crawler/reddit_web_crawler.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup
+
 import codecs
 import json
 import os
@@ -8,20 +10,31 @@ import requests
 class RedditWebCrawler(object):
 
 
-    REDDIT_URL = 'https://www.zhihu.com/'
+    REDDIT_URL = 'https://www.reddit.com/'
 
-    """docstring for ZhihuWebCrawler"""
-    def __init__(self, cmdline=None):
+    """docstring for RedditWebCrawler"""
+    # def __init__(self, cmdline=None):
 
 
     def parse_articles(self, board, path='.', timeout=5):
         filename = board + '.json'
         filename = os.path.join(path, filename)
         print('Processing:')
+        headers = {'User-agent': 'Mozilla/5.0'}
         resp = requests.get(
-            url = self.REDDIT_URL + '/topic#' + board,
-
+            url = self.REDDIT_URL + 'search?q=' + board,
+            timeout=timeout, headers=headers
         )
+
+        soup = BeautifulSoup(resp.text, 'lxml')
+        # print(soup.text)
+        for scrollerItem in soup.select('.scrollerItem div div div div span a'):
+            print('----------------------------new----------------------------')
+            print(scrollerItem)
+            print('----------------------------all----------------------------')
+            print(scrollerItem['href'])
+
+
         self.store(filename, u'{"articles": [', 'w')
 
         self.store(filename, u']}', 'a')
@@ -39,3 +52,7 @@ class RedditWebCrawler(object):
     def get(filename, mode='r'):
         with codecs.open(filename, mode, encoding='utf-8') as f:
             return json.load(f)
+
+if __name__ == '__main__':
+    r = RedditWebCrawler()
+    r.parse_articles(board='Warframe')
