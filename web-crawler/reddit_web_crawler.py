2a8a85cf83f01eecdbe5f7b45a1f9f4b3d021eac
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-09 23:43:57 +0900

    add picture_url

--- web-crawler/reddit_web_crawler.py
+++ web-crawler/reddit_web_crawler.py
@@ -2,46 +2,74 @@
 
 from bs4 import BeautifulSoup
 
+from flask import *
 import codecs
 import json
 import os
 import requests
 
+# Flask
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/WebCrawler/<web>/<board>')
+def index(web, board):
+    web_crawler = web_to_crawler(web)
+
+    return render_template('articles.html', articles=web_crawler.parse_articles(board=board), board=board)
+
+
+def web_to_crawler(web):
+    switcher = {
+        'reddit' : RedditWebCrawler
+    }
+
+    crawler = switcher.get(web)
+    return crawler()
+
+
+# Web Crawler
 class RedditWebCrawler(object):
 
-
-    REDDIT_URL = 'https://www.reddit.com/'
+    REDDIT_URL = 'https://www.reddit.com'
 
     """docstring for RedditWebCrawler"""
     # def __init__(self, cmdline=None):
 
 
-    def parse_articles(self, board, path='.', timeout=5):
+    def parse_articles(self, board, path='.', timeout=15):
         filename = board + '.json'
         filename = os.path.join(path, filename)
         print('Processing:')
         headers = {'User-agent': 'Mozilla/5.0'}
         resp = requests.get(
-            url = self.REDDIT_URL + 'search?q=' + board,
+            url = self.REDDIT_URL + '/search?q=' + board,
             timeout=timeout, headers=headers
         )
 
         soup = BeautifulSoup(resp.text, 'lxml')
         # print(soup.text)
-        for scrollerItem in soup.select('.scrollerItem div div div div span a'):
-            print('----------------------------new----------------------------')
-            print(scrollerItem)
-            print('----------------------------all----------------------------')
-            print(scrollerItem['href'])
 
+        articles = list()
+        for scrollerItem in soup.select('.scrollerItem div div div div'):
+            if scrollerItem.find(name='span'):
+                if scrollerItem.find(name='span').find(name='a'):
+                    article_content = scrollerItem.find(name='span').find(name='a')
+                    article = dict()
+                    article['name'] = article_content.find(name='span').get_text()
+                    article['link'] = self.REDDIT_URL + article_content['href']
+                    if len(scrollerItem.find_all(name='a')) > 1:
+                        if 'jpg' in scrollerItem.find_all(name='a')[1]['href'] or 'png' in scrollerItem.find_all(name='a')[1]['href']:
+                            article['picture_url'] = scrollerItem.find_all(name='a')[1]['href']
+                        else:
+                            article['picture_url'] = ''
+                    else:
+                        article['picture_url'] = ''
+                    articles.append(article)
 
-        self.store(filename, u'{"articles": [', 'w')
-
-        self.store(filename, u']}', 'a')
-        return filename
+        return articles
 
     # def parse_article(self, link, board):
-
 
     @staticmethod
     def store(filename, data, mode):
@@ -53,6 +81,8 @@ class RedditWebCrawler(object):
         with codecs.open(filename, mode, encoding='utf-8') as f:
             return json.load(f)
 
+
 if __name__ == '__main__':
-    r = RedditWebCrawler()
-    r.parse_articles(board='Warframe')
+    app.run(port=5000, debug=True)
+
+
