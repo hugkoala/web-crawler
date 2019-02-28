408511a8f8fe6e21df1cc6ddb31ef65a8928eb15
hugkoala <bmwbmwtruck1@gmail.com>
2019-03-01 01:04:29 +0900

    Update ck101_web_crawler.py

--- web-crawler/ck101_web_crawler/ck101_web_crawler.py
+++ web-crawler/ck101_web_crawler/ck101_web_crawler.py
@@ -1,4 +1,4 @@
-import requests;
+import requests, logging, time, random;
 from bs4 import BeautifulSoup as bs;
 
 BASE_URL = 'https://ck101.com/forum.php?mod=forumdisplay&fid=3419'
@@ -6,6 +6,27 @@ BASE_URL = 'https://ck101.com/forum.php?mod=forumdisplay&fid=3419'
 type_dict = {0: '', 1: '2399', 2: '2400', 3: '2401', 4: '2402', 5: '2403', 6: '2404', 7: '2405'}
 dateline_dict = {0: '', 1: '86400', 2: '172800', 3: '604800', 4: '2592000', 5: '7948800'}
 orderby_dict = {0: 'dateline', 1: 'replies', 2: 'views'}
+
+
+def load_web(url):
+    logging.info('===Load Web:{0}==='.format(url))
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4'
+    }
+    web_res = requests.get(url, headers=headers)
+    web_res.encoding = 'utf8'
+    time.sleep(random.uniform(0, 2))
+    web_bs = bs(web_res, 'lxml')
+    return web_bs
+
+
+def get_all_page_count(web_bs):
+    fd_page_bottom = web_bs.find('span', id='fd_page_bottom')
+    last_tag = fd_page_bottom.select_one('div').find('a', attrs={'class': 'last'}).text
+    return int(last_tag.split(' ')[1])
 
 
 class CK101WebCrawler(object):
@@ -26,7 +47,6 @@ class CK101WebCrawler(object):
         self.novel_orderby = novel_orderby
         self.file_dir = file_dir
 
-
     def get_url_with_args(self):
         url_with_args = BASE_URL
         url_with_args += '&typeid=' + type_dict.get(self.novel_type)
@@ -35,7 +55,21 @@ class CK101WebCrawler(object):
             url_with_args += '&filter=author&orderby=' + str(orderby_dict.get(self.novel_orderby))
         else:
             url_with_args += '&filter=reply&orderby=' + str(orderby_dict.get(self.novel_orderby))
-
+        return url_with_args
 
     def get_finished_novel(self):
-        novel_bs = bs(self.get_url_with_args())
+        logging.info('===Get Novel Page===')
+        url = self.get_url_with_args()
+        novel_bs = load_web(url)
+
+        all_page_count = get_all_page_count(novel_bs)
+
+        for i in range(all_page_count):
+            
+
+
+
+
+
+
+
