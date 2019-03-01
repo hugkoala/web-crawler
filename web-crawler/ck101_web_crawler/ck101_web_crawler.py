1df82af366319d41c4cf1f77a8567330d037e408
hugkoala <bmwbmwtruck1@gmail.com>
2019-03-01 18:16:00 +0900

    Update ck101_web_crawler.py

--- web-crawler/ck101_web_crawler/ck101_web_crawler.py
+++ web-crawler/ck101_web_crawler/ck101_web_crawler.py
@@ -1,4 +1,4 @@
-import requests, time, random, re;
+import requests, time, random, re, codecs, os, errno;
 from bs4 import BeautifulSoup as bs;
 
 BASE_URL = 'https://ck101.com/forum.php?mod=forumdisplay&fid=3419'
@@ -23,6 +23,7 @@ def load_web(url):
     return web_bs
 
 
+# Get All Novel Page Count
 def get_all_page_count(web_bs):
     fd_page_bottom = web_bs.find('span', id='fd_page_bottom')
     if fd_page_bottom.text == '':
@@ -30,6 +31,41 @@ def get_all_page_count(web_bs):
     else:
         last_tag = fd_page_bottom.select_one('div').find('a', attrs={'class': 'last'}).text
         return int(last_tag.split(' ')[1])
+
+
+# Get a Novel Page Count
+def get_novel_page_count(novel_bs):
+    pgt = novel_bs.find('div', id='pgt')
+    pg_class = pgt.find('div', class_='pgt').find('div', class_='pg')
+    last_tag = pg_class.find('a', attrs={'class': 'last'})
+    if last_tag:
+        return int(last_tag.text.split(' ')[1])
+    else:
+        return int(pgt.find('div', class_='pgt').find('div', class_='pg').find_all('a')[-2].text)
+
+
+def get_novel_title(novel_bs):
+    return novel_bs.find('h1', id='thread_subject').text
+
+
+def create_dir(filepath):
+    if not os.path.exists(os.path.dirname(filepath)):
+        try:
+            os.makedirs(os.path.dirname(filepath))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+
+
+def store_file(filename, data, mode):
+    create_dir(filename)
+    with codecs.open(filename, mode, encoding='utf-8') as f:
+        f.write(data)
+
+
+def read_file(filename, mode='r'):
+    with codecs.open(filename, mode, encoding='utf-8') as f:
+        return f.read()
 
 
 class CK101WebCrawler(object):
@@ -60,19 +96,39 @@ class CK101WebCrawler(object):
             url_with_args += '&filter=reply&orderby=' + str(orderby_dict.get(self.novel_orderby))
         return url_with_args
 
+    def get_novel(self, url):
+        novel_bs = load_web(url)
+        novel_page_count = get_novel_page_count(novel_bs)
+        novel_title = get_novel_title(novel_bs)
+        novel_path = os.path.join(self.file_dir, novel_title + '.txt')
+        print('Novel Title:', novel_title)
+        print('Page Count:', novel_page_count)
+
+        store_file(filename=novel_path, data=novel_title, mode='w+')
+        for i in range(novel_page_count):
+            novel_info_bs = load_web(url + '&page=' + str(i + 1))
+
+            for novel_post in novel_info_bs.find_all('td', id=re.compile('^postmessage_')):
+                store_file(filename=novel_path, data='\n' + novel_post.text, mode='a')
+
     def get_finished_novel(self):
         print('===Get Novel Page===')
         url = self.get_url_with_args()
         all_page_bs = load_web(url)
 
         all_page_count = get_all_page_count(all_page_bs)
+        novel_link_list = []
 
+        # Get All Novel Link
         for i in range(all_page_count):
             novel_bs = load_web(url + '&page=' + str(i + 1))
-            novel_list = novel_bs.find('table', id='threadlisttableid').findAll('tbody', id=re.compile('^normalthread_'))
-            for idx, item in enumerate(novel_list):
-                novel_link = item.find('div', class_='blockTitle').find('a', class_='s xst')['href']
-                print(novel_link)
+            novel_page_bs = novel_bs.find('table', id='threadlisttableid').findAll('tbody', id=re.compile('^normalthread_'))
+
+            for novel in novel_page_bs:
+                novel_link_list.append(novel.find('div', class_='blockTitle').find('a', class_='s xst')['href'])
+
+        for novel_link in novel_link_list:
+            self.get_novel(novel_link)
 
 
 
